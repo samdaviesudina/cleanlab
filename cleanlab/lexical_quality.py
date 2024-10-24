0152a0641a1e8c5137573f60b948f20f2341fd14
Sam Davies Udina <sam.daviesudina@sensat.co.uk>
2024-10-24 13:58:16 +0100

    Start with a silly hello-world based on text length

---
+++ cleanlab/lexical_quality.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def calculate_lexical_quality_scores(texts: np.ndarray) -> pd.DataFrame:
+    vectorized_sigmoid = np.vectorize(sigmoid)
+    vectorized_lexical_quality_score_calculation = np.vectorize(calculate_lexical_quality_score)
+
+    return vectorized_sigmoid(vectorized_lexical_quality_score_calculation(texts))
+
+
+def calculate_lexical_quality_score(text: str) -> float:
+    print(len(text))
+    return len(text)
